36c9a52f5e266396ab1be61e099a9fa3bc28fa12
Billy <billy@room317.tech>
2025-02-18 10:44:23 -0600

    Did i fix it?

---
+++ WatchDoge/ApiClient/DogeClient.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using WatchDoge.Model;
+
+namespace WatchDoge.ApiClient;
+
+public class DogeClient
+{
+    private const string BASE_URL = "https://doge.gov";
+    private const string SNAPSHOT_BASE_URL = "https://pub-1234b018e8b4435b92575fac089aa86f.r2.dev";
+    private readonly HttpClient _client = new();
+
+    public async Task<Receipts?> GetReceipts()
+    {
+        const string endpoint = BASE_URL + "/api/receipts/overview";
+        var response = await _client.GetAsync(endpoint);
+
+        var responseString = await response.Content.ReadAsStringAsync();
+        return JsonConvert.DeserializeObject<Receipts>(responseString);
+    }
+}
